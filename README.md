db23c7a6400b361f37ed6d5e9a2cd8e8f742c678
John Gotschall <contact@johnjg.com>
2016-07-15 21:54:07 -0400

    add image hover listener and action

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 https://github.com/johnjg/hw_listeners_gotschall_john.git
 
 # Resources:
-I did not use any resources outside of the lectures and my notes on this assignment.
+- The following resource was used to learn more about creating new HTML elements with JavaScript.
+- https://developer.mozilla.org/en-US/docs/Web/API/Document/createElement
+- The following resource was used to learn more about retrieving the value of an input field.
+- http://www.w3schools.com/jsref/prop_text_value.asp
+- The following resource was used to learn more about event types
+- https://developer.mozilla.org/en-US/docs/Web/Events
 
 # Comments:
+I do not have any comments about this assignment.
--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
-// The following code targets the contact form and listens for the submit event, interrupts the form's default submit function, and generates an HTML message that encorporates the form field input.
+// The following code targets the contact form and listens for the submit event,
+// interrupts the form's default submit function,
+// and generates an HTML message that encorporates the form field input.
 
 var theForm = document.getElementById('contact');
 
@@ -9,7 +11,9 @@ theForm.addEventListener('submit', function (eventInfo) {
     document.getElementsByClassName('container')[0].appendChild(myElement);
 });
 
-// The following code targets the hyperlink and listens for the click event, interrupts the link's default function, and changes the content of the header H1 element.
+// The following code targets the hyperlink and listens for the click event,
+// interrupts the link's default function,
+// and changes the content of the header H1 element.
 
 var theLink = document.getElementById('link');
 
@@ -17,4 +21,17 @@ theLink.addEventListener('click', function (clickEvent) {
     clickEvent.preventDefault();
     var newHeader = document.getElementById('header');
     newHeader.innerHTML = 'I told you not to click the link! Now you messed up my header!';
+});
+
+// The following code targets the button image and listens for the mouseover event,
+// changes the content of the header H1 element to it's original state
+// and replaces the button image with a new image file.
+
+var theImage = document.getElementById('image');
+
+theImage.addEventListener('mouseover', function (hoverEvent) {
+    hoverEvent.preventDefault();
+    var hoverReset = document.getElementById('header');
+    hoverReset.innerHTML = 'This is my Header!';
+    document.getElementById('image').src = 'img/greenbutton.png';
 });
